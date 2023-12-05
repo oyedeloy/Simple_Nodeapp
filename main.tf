27e7626f2a4ae085c32c603ddf0ea7a3c092ce06
dele <oyedeloy@gmail.com>
2023-12-05 08:07:06 -0500

    main.tf added

--- main.tf
+++ main.tf
@@ -88,5 +88,5 @@ provisioner "local-exec" {
 
 # Output the public IP address of the EC2 instance
 output "public_ip" {
-  value = aws_instance.Java_web.public_ip
+  value = aws_instance.ASI_web.public_ip
 }
